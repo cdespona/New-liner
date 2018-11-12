60766b336f6dd950aff1e3291e6a77fb7da2402e
Carles D. Espona <carles.espona@mango.com>
2018-11-12 12:00:42 +0100

    Remove extra line

--- src/test/java/cde/cw/SplitSentenceShould.java
+++ src/test/java/cde/cw/SplitSentenceShould.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SplitSentenceShould {
-    
+
     private SplitSentence splitSentence;
 
     @BeforeEach
